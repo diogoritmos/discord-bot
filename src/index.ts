116bf5143eb1c71bd008a90afa1bc81b5bf05dd1
Diogo Dourado <diogo.dourado@quintoandar.com.br>
2025-04-11 18:10:09 -0300

    Setup youtube video fetcher

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import youtube from "./youtube";
+
 export default {
 	async fetch(req) {
 		const url = new URL(req.url);
@@ -8,18 +10,9 @@ export default {
 		);
 	},
 
-	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
-	// [[triggers]] configuration.
 	async scheduled(event, env, ctx): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		const resp = await fetch("https://api.cloudflare.com/client/v4/ips");
-		const wasSuccessful = resp.ok ? "success" : "fail";
+		const latestVideo = (await youtube.getRecentVideos(env.YOUTUBE_CHANNEL_ID))[0];
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		console.log(latestVideo);
 	},
 } satisfies ExportedHandler<Env>;
---
+++ src/youtube.ts
@@ -0,0 +1,47 @@
+import { XMLParser } from "fast-xml-parser";
+
+interface YoutubeVideoEntry {
+	id: string;
+	"yt:videoId": string;
+	"yt:channelId": string;
+	title: string;
+	link: "string";
+	author: {
+		name: string;
+		uri: string;
+	};
+	published: string;
+	updated: string;
+	"media:group": {
+		"media:title": string;
+		"media:content": string;
+		"media:thumbnail": string;
+		"media:description": string;
+		"media:comunity": object;
+	};
+}
+
+interface Video {
+	title: string;
+	url: string;
+	tsPublished: Date;
+}
+
+const getRecentVideos = async (channelId: string, maxResults: number = 1): Promise<Video[]> => {
+	const feedUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${channelId}`;
+	const res = await fetch(feedUrl);
+	const text = await res.text();
+
+	const parser = new XMLParser();
+	const entries = parser.parse(text).feed.entry as YoutubeVideoEntry[];
+
+	return entries
+		.map((entry) => ({
+			title: entry.title,
+			url: `https://www.youtube.com/watch?v=${entry["yt:videoId"]}`,
+			tsPublished: new Date(entry.published),
+		}))
+		.slice(0, maxResults); // They're already sorted by published date
+};
+
+export default { getRecentVideos };
